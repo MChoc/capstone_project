d8d0e8e5e5273de90aa331b239193b4dfb5592f0
Colin <colin.yang@wsp.com>
2020-03-06 10:40:58 +1100

    Example starting code for menu models

--- waiting/menu/models.py
+++ waiting/menu/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+
+
+class Menu(models.Model):
+    pass
