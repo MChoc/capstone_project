3a41749c8da943db6a08c3a53212c44f9596a6e8
Kaur, Tripta <tripta.kaur@accenture.com>
2020-03-15 02:00:19 +1100

    registration with rest-api now possible

--- waiting/waiting/urls.py
+++ waiting/waiting/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     path('api/', include(router.urls)),
     # path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
