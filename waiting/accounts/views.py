5b0176292e2a3802fc211f38a937b548e809c9cc
Kaur, Tripta <tripta.kaur@accenture.com>
2020-03-15 02:01:13 +1100

    enable get requests without authorization except for  accounts

--- waiting/accounts/views.py
+++ waiting/accounts/views.py
@@ -1,9 +1,10 @@
 from . import serializers
 
 from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
--- waiting/waiting/settings.py
+++ waiting/waiting/settings.py
@@ -55,7 +55,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     )
 }
 
