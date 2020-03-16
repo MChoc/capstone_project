bc799445977fbcf9c26f23cb515f727956b492a8
Tripta Kaur <31278242+tripta96@users.noreply.github.com>
2020-03-16 12:26:48 +1100

    Revert "Dev feature allow registration tripta"

--- waiting/waiting/urls.py
+++ waiting/waiting/urls.py
@@ -18,17 +18,15 @@ from django.urls import include, path
 from rest_framework import routers
 
 from menu.urls import router as menu_router
-from accounts.urls import router as accounts_router
-
+from users.urls import router as users_router
 
 router = routers.DefaultRouter()
 router.registry.extend(menu_router.registry)
-router.registry.extend(accounts_router.registry)
+router.registry.extend(users_router.registry)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
