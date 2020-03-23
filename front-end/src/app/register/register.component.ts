971f1882e0d1bb10e6a86a0efa8fd490afe8df0b
Lauren Klimt <z5059785@student.unsw.edu.au>
2020-03-23 17:59:43 +1100

    working on API

--- front-end/src/app/register/register.component.ts
+++ front-end/src/app/register/register.component.ts
@@ -8,9 +8,40 @@ import { HttpClient } from '@angular/common/http';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
+
+  fname: string;
+  username: string;
+  lname: string;
+  password1: string;
+  password2: string;
+  employeeType: string;
+
+  url = 'http://127.0.0.1:5000/rest-auth/registration/';
+
+  registerUser() {
+    let post_data = {
+      username: this.username,
+      password1: this.password1,
+      password2: this.password2,
+      first_name: this.fname,
+      last_name: this.lname,
+      user_type: this.employeeType
+    };
+    console.log("request: " + post_data);
+    this.http.post(this.url, post_data).toPromise().then(data => {
+      console.log("rresponse!:");
+      console.log(data);
+      console.log(data['key']);
+    },
+    error=> {
+      // TODO: show error message on page!
+      console.log(error.error);
+    });
+  }
+
 
 }
