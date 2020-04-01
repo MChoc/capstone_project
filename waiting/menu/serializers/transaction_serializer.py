5b2b8b9b9d25eadda4ae5700bf2df26a0c8eb6f3
Colin Yang <colin.yang96@gmail.com>
2020-04-01 18:49:43 +1100

    Move many-to-many relationship between food item and transaction to transaction model

--- waiting/menu/serializers/transaction_serializer.py
+++ waiting/menu/serializers/transaction_serializer.py
@@ -4,6 +4,11 @@ from rest_framework import serializers
 
 
 class TransactionSerializer(serializers.HyperlinkedModelSerializer):
+    food_items = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='fooditem-detail'
+    )
     class Meta:
         model = Transaction
-        fields = ['id', 'url', 'date', 'customer', 'credit_card']
+        fields = ['id', 'url', 'date', 'customer', 'credit_card', 'food_items']
