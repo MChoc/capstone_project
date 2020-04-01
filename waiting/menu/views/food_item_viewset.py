09cf1aaec7aa09a008a9030270c1bafb5ad590cf
Tripta Kaur <t.kaur@unsw.edu.au>
2020-04-01 13:43:27 +1100

    Customer permissions added

--- waiting/menu/views/food_item_viewset.py
+++ waiting/menu/views/food_item_viewset.py
@@ -2,8 +2,10 @@ from menu.models.food_item import FoodItem
 from menu.serializers.food_item_serializer import FoodItemSerializer
 
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class FoodItemViewSet(viewsets.ModelViewSet):
     queryset = FoodItem.objects.all()
     serializer_class = FoodItemSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
