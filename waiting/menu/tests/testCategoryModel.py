cce4d297f159ccdf8138961853dd7f402eb9fe6b
Colin Yang <colin.yang96@gmail.com>
2020-04-04 23:05:42 +1100

    Initial tests

--- waiting/menu/tests/testCategoryModel.py
+++ waiting/menu/tests/testCategoryModel.py
@@ -1,41 +1,77 @@
-from menu.models.menu import Menu
-from menu.models.category import Category
-from menu.serializers.category_serializer import CategorySerializer
+from collections import OrderedDict
 
-from django.contrib.auth.models import User
+from menu.models.category import Category
+from menu.models.category import Menu
+from menu.serializers.category_serializer import CategorySerializer
+from menu.serializers.menu_serializer import MenuSerializer
+
+from django.contrib.auth import get_user_model
+from django.core import serializers
+
 from rest_framework import status
-from rest_framework.authtoken.models import Token
+from rest_framework.reverse import reverse
 from rest_framework.request import Request
 from rest_framework.test import APITestCase, APIRequestFactory
 
 
-class TestFoodItemModel(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
-        fixtures = ['initial_db.json']
+class TestCategoryModel(APITestCase):
+    """
+    Testing the Menu model and its API returns
+
+    FYI
+        GET: LIST/RETRIEVE
+        POST: CREATE
+        PUT/PATCH: UPDATE
+        DELETE: DESTROY
+
+    LIST:
+        Lists a queryset.
+
+    RETRIEVE:
+        Retrieve a model instance.
+
+    CREATE:
+        Create a model instance.
+    
+    UPDATE:
+        Update a model instance.
+    
+    DESTROY:
+        Destroy a model instance.
+    """
+    fixtures = ['dump.json']
 
     def setUp(self):
         login_url = '/rest-auth/login/'
-        content = {'username': 'Customer1', 'password': 'Customer1'}
-        response = self.client.post(login_url, data=content, format='json')
-        print(response)
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + response.data['key'])
+        body = {'username': 'Customer0', 'password': 'Customer0'}
+        response = self.client.post(login_url, body, format='json')
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Token ' + response.data['key']
+        )
 
     def tearDown(self):
         logout_url = '/rest-auth/logout/'
-        response = self.client.post(logout_url)
+        self.client.post(logout_url)
 
+    """
+    Testing LIST:
+        Lists a queryset.
+    
+    Checks for:
+        200 response.
+        GET data is same as database data.
+    """
     def test_list(self):
         url = '/api/categories/'
         factory = APIRequestFactory()
         request = factory.post(url)
         
-        food_items = Category.objects.all()
+        categories = Category.objects.all()
         serializer_context = {
             'request': Request(request)
         }
         serializer = CategorySerializer(
-            food_items,
+            categories,
             context=serializer_context,
             many=True
         )
@@ -44,3 +80,130 @@ class TestFoodItemModel(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
+
+    """
+    Testing CREATE
+        Create a model instance.
+    
+    Asserts:
+        201 response.
+        Object count increased.
+        Object exists in database.
+    """
+    def test_create(self):
+        url = '/api/categories/'
+        factory = APIRequestFactory()
+        request = factory.post(url)
+        
+        init_count = Category.objects.count()
+
+        body = {
+            'name': 'Test Category',
+            'active': True,
+            'menu': reverse(
+                'menu-detail',
+                args=[Menu.objects.get(id=1).pk],
+                request=request
+            )
+        }
+        response = self.client.post(url, body, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        post_count = Category.objects.count()
+        self.assertEqual(post_count, init_count+1)
+
+        post_obj = Category.objects.get(name='Test Category')
+        self.assertIsNotNone(post_obj)
+
+    """
+    Testing RETRIEVE
+        Retrieve a model instance.
+    
+    Asserts:
+        200 response.
+        GET data is same as in database.
+    """
+    def test_retrieve(self):
+        url = '/api/categories/1/'
+        factory = APIRequestFactory()
+        request = factory.post(url)
+        
+        categories = [Category.objects.get(id=1)]
+        serializer_context = {
+            'request': Request(request)
+        }
+        serializer = CategorySerializer(
+            categories,
+            context=serializer_context,
+            many=True
+        )
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        self.assertEqual([OrderedDict(response.data)], serializer.data)
+        
+    """
+    Testing UPDATE
+        Update a model instance.
+    
+    Asserts:
+        200 response.
+        All fields have been changed and content is correct.
+    """
+    def test_update(self):
+        url = '/api/categories/1/'
+        factory = APIRequestFactory()
+        request = factory.post(url)
+        
+        menu = Menu.objects.create(name='Test Menu')
+        menu_url = reverse('menu-detail', args=[menu.pk], request=request)
+        body = {
+            'name': 'Category Change',
+            'active': False,
+            'menu': menu_url
+        }
+        response = self.client.put(url, body, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        category = Category.objects.get(id=1)
+        self.assertEqual(category.name, 'Category Change')
+        self.assertFalse(category.active)
+        self.assertEqual(category.menu, menu)
+
+    """
+    Testing UPDATE (partial)
+        Partially update a model instance.
+
+    Asserts:
+        200 response.
+        Correct field/s have been changed and content correct.
+    """
+    def test_partial_update(self):
+        url = '/api/categories/1/'
+        factory = APIRequestFactory()
+        request = factory.post(url)
+
+        body = {
+            'name': 'Test Category'
+        }
+        response = self.client.patch(url, body, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        category = Category.objects.get(id=1)
+        self.assertEqual(category.name, 'Test Category')
+
+    """
+    Testing DESTROY
+        Destroy a model instance.
+    
+    Asserts:
+        204 response.
+        Correct object has been deleted from database.
+    """
+    def test_destroy(self):
+        url = '/api/categories/1/'
+
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertRaises(Category.DoesNotExist, Category.objects.get, id=1)
