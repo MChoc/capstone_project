95e01b4dc95aa16b3f768ecb56467bd5991315cc
Colin Yang <colin.yang96@gmail.com>
2020-04-05 19:28:01 +1000

    changed where extras resides

--- waiting/menu/models/extra.py
+++ waiting/menu/models/extra.py
@@ -6,7 +6,6 @@ from django.db import models
 class Extra(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    food_item = models.ManyToManyField(FoodItem)
 
     def __str__(self):
         return self.name
