a451a341a693fc671b904b6025d276bbe65db961
Colin Yang <colin.yang96@gmail.com>
2020-04-05 16:41:13 +1000

    Added manytomany relation with food item in extras so FE can filter out relevant extras

--- waiting/menu/models/extra.py
+++ waiting/menu/models/extra.py
@@ -1,9 +1,12 @@
+from menu.models.food_item import FoodItem
+
 from django.db import models
 
 
 class Extra(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    food_item = models.ManyToManyField(FoodItem)
 
     def __str__(self):
         return self.name
