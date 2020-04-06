aacf4de8d9637c9255146fefd8e3cfb02c16b9ae
z5157382 <z5157382@student.unsw.edu.au>
2020-04-06 11:57:44 +0930

    added all files

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Angus\Desktop\Code Files\capstone-project-waiting-for-the-train\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
