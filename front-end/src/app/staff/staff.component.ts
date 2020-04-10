57af1d702d7c212014ffae913f143b1507ed2b5d
leonardlwq <leonardlwq@gmail.com>
2020-04-10 13:37:16 +1000

    added conflicts

--- front-end/src/app/staff/staff.component.ts
+++ front-end/src/app/staff/staff.component.ts
@@ -21,6 +21,11 @@ export class StaffComponent implements OnInit {
     private http: HttpClient,
     private data : DataService,
     private router: Router) { 
+      let loggedOn = window.localStorage.getItem('user');
+
+      if(!loggedOn || JSON.parse(loggedOn)['user_type'] != 'MANAGER') {
+        this.router.navigate(['**']);
+      }  
     router.events.subscribe((_: NavigationEnd) => this.currentUrl = _.url)
   }
 
