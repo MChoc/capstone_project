60d031ad558814948ec8ba0526a97d96f5e894d5
Lauren Klimt <z5059785@student.unsw.edu.au>
2020-04-11 20:38:44 +1000

    customer return home

--- front-end/src/app/assistance-id/assistance-id.component.ts
+++ front-end/src/app/assistance-id/assistance-id.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-assistance-id',
@@ -11,13 +11,18 @@ export class AssistanceIdComponent implements OnInit {
   id: String;
 
   constructor(
-    private _Activatedroute: ActivatedRoute
+    private _Activatedroute: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this._Activatedroute.paramMap.subscribe(params => { 
       this.id = params.get('id');
     });
+  }
+
+  menu() {
+    this.router.navigate(['/'])
   }
 
 }
