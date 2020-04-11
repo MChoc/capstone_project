cb66005a13dacdfa7312d7a054efda13bddb6739
Tripta Kaur <t.kaur@unsw.edu.au>
2020-04-12 00:28:08 +1000

    Add prepared field to transaction

--- waiting/menu/models/transaction.py
+++ waiting/menu/models/transaction.py
@@ -7,6 +7,7 @@ from django.db import models
 
 class Transaction(models.Model):
     active = models.BooleanField(default=True)
+    prepared = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
     customer = models.ForeignKey(
         CustomUser,
