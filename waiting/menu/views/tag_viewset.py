75d933dfb2d8c620ed6e1aa3e293e6f5ca3988fa
Colin Yang <colin.yang96@gmail.com>
2020-04-11 16:50:20 +1000

    Read only

--- waiting/menu/views/tag_viewset.py
+++ waiting/menu/views/tag_viewset.py
@@ -2,8 +2,11 @@ from menu.models.tag import Tag
 from menu.serializers.tag_serializer import TagSerializer
 
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
