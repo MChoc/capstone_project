6ace18ca0672dab152cc1dd79f99c5d704d0937f
Lauren Klimt <z5059785@student.unsw.edu.au>
2020-04-14 15:32:19 +1000

    manager assistance details page

--- front-end/src/app/manager-alerts-assistance/manager-alerts-assistance.component.ts
+++ front-end/src/app/manager-alerts-assistance/manager-alerts-assistance.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-manager-alerts-assistance',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manager-alerts-assistance.component.css']
 })
 export class ManagerAlertsAssistanceComponent implements OnInit {
+  id: string;
+  request: Object;
+  request_problem: string;
+  request_id: string;
 
-  constructor() { }
+  constructor(
+    private _Activatedroute: ActivatedRoute, 
+    private http: HttpClient, 
+    private router: Router
+  ) { 
+    let loggedOn = window.localStorage.getItem('user');
+
+      if(!loggedOn || JSON.parse(loggedOn)['user_type'] != 'MANAGER') {
+        this.router.navigate(['**']);
+      }
+  }
 
   ngOnInit(): void {
+    this._Activatedroute.paramMap.subscribe(params => { 
+      this.id = params.get('id');
+  });
+  let url = 'http://127.0.0.1:5000/api/assistance/' + this.id + '/';
+    let key = window.localStorage.getItem('key')
+    let header = {
+      headers: new HttpHeaders()
+        .set('Authorization', 'Token ' + key)
+    }
+    this.http.get(url, header).toPromise().then(data => {
+      this.request = data;
+      this.request_id = data['id'];
+      this.request_problem = data['problem'];
+    },
+    error => {
+      console.log("ERROR!")
+      console.log(error.error);
+    })
+  }
+
+  back() {
+    this.router.navigate(['/management/alerts']);
   }
 
 }
