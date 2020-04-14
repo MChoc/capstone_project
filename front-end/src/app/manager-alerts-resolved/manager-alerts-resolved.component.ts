6490825fca7d8d9bf4447a69daf3372f05fbf0af
Lauren Klimt <z5059785@student.unsw.edu.au>
2020-04-14 16:25:45 +1000

    resolved alert page shows resolved alerts

--- front-end/src/app/manager-alerts-resolved/manager-alerts-resolved.component.ts
+++ front-end/src/app/manager-alerts-resolved/manager-alerts-resolved.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DataService } from '../_services/data.service';
+import { Router, NavigationEnd } from '@angular/router';
+import { interval } from 'rxjs';
+import { startWith, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-manager-alerts-resolved',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagerAlertsResolvedComponent implements OnInit {
 
-  constructor() { }
+  currentUrl: string;
+  requests$: Object;
+  transactions$: Object;
+
+  constructor(private http: HttpClient,
+    private data : DataService,
+    private router: Router) { 
+      let loggedOn = window.localStorage.getItem('user');
+
+      if(!loggedOn || JSON.parse(loggedOn)['user_type'] != 'MANAGER') {
+        this.router.navigate(['**']);
+      }  
+    router.events.subscribe((_: NavigationEnd) => this.currentUrl = _.url)
+  }
 
   ngOnInit(): void {
+    interval(10000)
+    .pipe(
+      startWith(0),
+      switchMap(() => this.data.getRequests())
+    )
+    .subscribe(res => {
+      this.requests$ = res;
+    });
+
+    interval(10000)
+      .pipe(
+        startWith(0),
+        switchMap(() => this.data.getTransactions())
+      )
+      .subscribe(res => {
+        this.transactions$ = res;
+      });
+  }
+
+  back() {
+    this.router.navigate(['/management/alerts']);
   }
 
 }
