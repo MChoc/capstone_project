5c68a76157c97a8bf644e63806f1afceee1e6667
suttor32 <30674981+suttor32@users.noreply.github.com>
2020-04-15 10:10:20 +0930

    Fixed bug in filter

--- front-end/src/app/filter.pipe.ts
+++ front-end/src/app/filter.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 
 export class FilterPipe implements PipeTransform {
-  transform(items$: any[], searchText: string): any[] {
+  transform(items$: any, searchText: string): any {
     if(!items$) return [];
     if(!searchText) return items$;
 searchText = searchText.toLowerCase();
