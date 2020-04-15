192a196d5e38a3293bd3b60ed8842417209540d4
Tripta Kaur <t.kaur@unsw.edu.au>
2020-04-15 22:44:57 +1000

    Add time filter to transactions

--- waiting/menu/views/transaction_viewset.py
+++ waiting/menu/views/transaction_viewset.py
@@ -1,3 +1,6 @@
+import dateutil.parser
+from datetime import datetime
+
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -21,6 +24,12 @@ class TransactionViewSet(ModelViewSet):
         Else execute default get_queryset method.
     """
     def get_queryset(self):
+        start_date = self.request.query_params.get('start_date')
+        if start_date:
+            start_date = dateutil.parser.parse(start_date)
+        else:
+            start_date = datetime(1970, 1, 1)
+
         if self.request.query_params.get('get_unprepared'):
-            return Transaction.objects.filter(prepared=False)
-        return super().get_queryset()
+            return Transaction.objects.filter(prepared=False).exclude(date__lt=start_date)
+        return super().get_queryset().exclude(date__lt=start_date)
