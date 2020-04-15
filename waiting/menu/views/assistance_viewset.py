497cb39959c4d976ba5a4165be9461825adbddb7
Tripta Kaur <t.kaur@unsw.edu.au>
2020-04-15 23:03:33 +1000

    Add date filter to assistance requests

--- waiting/menu/views/assistance_viewset.py
+++ waiting/menu/views/assistance_viewset.py
@@ -1,3 +1,5 @@
+import dateutil.parser
+
 from rest_framework.viewsets import ModelViewSet
 from accounts.permissions import IsStaffOrPostOnly
 
@@ -9,3 +11,13 @@ class AssistanceViewSet(ModelViewSet):
     queryset = Assistance.objects.all()
     serializer_class = AssistanceSerializer
     permission_classes = [IsStaffOrPostOnly]
+
+    def get_queryset(self):
+
+        start_date = self.request.query_params.get('start_date')
+
+        if start_date:
+            start_date = dateutil.parser.parse(start_date)
+            return Assistance.objects.exclude(date__lt=start_date)
+
+        return super().get_queryset()
