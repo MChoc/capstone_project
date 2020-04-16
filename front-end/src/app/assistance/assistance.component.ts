3e9951bd2f198158f60f05d655b64964e9f822bf
leonardlwq <leonardlwq@gmail.com>
2020-04-16 20:58:12 +1000

    radioboxes

--- front-end/src/app/assistance/assistance.component.ts
+++ front-end/src/app/assistance/assistance.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-assistance',
@@ -10,44 +11,46 @@ import { Router } from '@angular/router';
 })
 export class AssistanceComponent implements OnInit {
 
-  requestlist = [
+  constructor(
+    private http: HttpClient, 
+    private router: Router,
+  ) {}
+
+  orders = [
     { id: 1, request: 'My table needs to be cleaned' },
     { id: 2, request: 'I need serviettes' },
     { id: 3, request: 'I need cutlery' },
     { id: 4, request: 'I need more condiments' }, 
     { id: 5, request: 'Problem with card payment' },
-    { id: 6, request: 'I cannot add items in my cart' }
-  ]
-
-  constructor(
-    private http: HttpClient, 
-    private router: Router
-  ) { }
+    { id: 6, request: 'I cannot add items in my cart' },
+    { id: 7, request: "Other requests"}
+  ];
 
   ngOnInit(): void {
-    this.addCheckboxes();
-  }
-
-  addCheckboxes() {
-    this.requestlist.forEach((o,i) => {
-      const control = new FormControl(i===0);
-      (this.assistanceForm.controls.requests as FormArray).push(control);
-    })
+    this.assistanceForm.get('issue').disable()
+    this.assistanceForm.controls.orders.patchValue(this.orders[0].id);
   }
 
   assistanceForm = new FormGroup({
     issue: new FormControl(),
-    requests: new FormArray([]),
+    orders: new FormControl({orders: ['']})
   });
 
+  get_request_string = (request_id) => {
+    return this.orders[request_id-1].request
+  }
+  
+  alertme = () => {
+    let id_current = this.assistanceForm.value['orders'];
+    if (id_current == this.orders.length) this.assistanceForm.get('issue').enable()
+    else this.assistanceForm.get('issue').disable()
+  }
+
   onFormSubmit() {
-    const selectedrequestids = this.assistanceForm.value.requests.map((v,i) => (v ? this.requestlist[i].id : null)).filter(v  => v !== null);
-    var string = ''
-    for (let i = 0; i < selectedrequestids.length; i++){
-      string += this.requestlist[selectedrequestids[i]-1].request
-      string += "\n"
-    };
-    if (this.assistanceForm.value['issue'] !== null) string += this.assistanceForm.value['issue']
+    let string = '';
+    let id_recieve = this.assistanceForm.value['orders'];
+    if (id_recieve != this.orders.length) string = this.get_request_string(id_recieve)
+    else string = this.assistanceForm.value['issue']
     let request_data = {
       'problem': string
     }
