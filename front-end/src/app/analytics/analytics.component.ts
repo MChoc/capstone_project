9d5c7b9cc6b5393e15a00c607474935e5bf343e2
Lauren Klimt <z5059785@student.unsw.edu.au>
2020-04-21 10:49:44 +1000

    can display stats for A category

--- front-end/src/app/analytics/analytics.component.ts
+++ front-end/src/app/analytics/analytics.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DataService } from '../_services/data.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-analytics',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalyticsComponent implements OnInit {
 
-  constructor() { }
+  currentUrl: string;
+  categories$: Object;
+  category1: Object;
+  items$: Object;
+  category_name: string;
+
+  constructor( 
+    private http: HttpClient,
+    private data : DataService,
+    private router: Router
+    ) {
+
+    let loggedOn = window.localStorage.getItem('user');
+
+    if(!loggedOn || JSON.parse(loggedOn)['user_type'] != 'MANAGER') {
+      this.router.navigate(['**']);
+    }
+
+    router.events.subscribe((_: NavigationEnd) => this.currentUrl = _.url)
+  }
 
   ngOnInit(): void {
+    this.data.getCategoryStats().subscribe(
+      data => this.categories$ = data,
+    ),
+    this.data.getCategoryData(1).subscribe(
+      data => {
+        this.category1 = data,
+        this.items$ = data['food_items'],
+        this.category_name = data['name']
+      }
+    )  
   }
 
 }
