37ab59feca1fa5fbc083c20e7d927ecd0e274c2a
leonardlwq <leonardlwq@gmail.com>
2020-04-22 21:46:36 +1000

    changed to checkboxes

--- front-end/src/app/assistance/assistance.component.ts
+++ front-end/src/app/assistance/assistance.component.ts
@@ -1,67 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { of } from 'rxjs';
-import { animation, transition, animate, state, trigger, style } from '@angular/animations';
-
 
 @Component({
   selector: 'app-assistance',
   templateUrl: './assistance.component.html',
-  styleUrls: ['./assistance.component.css'],
-  animations: [
-    trigger('fade', [
-      transition('void => *', [
-        style({backgroundColor: 'white', opacity: 0, transform: 'translateX(40px)'}),
-        animate(300)
-      ])
-    ])
-  ]
+  styleUrls: ['./assistance.component.css']
 })
 export class AssistanceComponent implements OnInit {
 
+  requestlist = [
+    { id: 1, request: 'My table needs to be cleaned' },
+    { id: 2, request: 'I need serviettes' },
+    { id: 3, request: 'I need cutlery' },
+    { id: 4, request: 'I need more condiments' }, 
+    { id: 5, request: 'Problem with card payment' },
+    { id: 6, request: 'I cannot add items in my cart' }
+  ]
+
   constructor(
     private http: HttpClient, 
-    private router: Router,
-  ) {}
-
-  orders = [
-    { id: 1, request: 'My table needs to be cleaned' },
-    { id: 2, request: 'I have a question about the menu' },
-    { id: 3, request: 'There is a problem with my credit card payment' },
-    { id: 4, request: 'I need help with my cart' },
-    { id: 5, request: 'There is a problem with my food' },
-    { id: 6, request: 'I need serviettes' },
-    { id: 7, request: 'I need cutlery' },
-    { id: 8, request: "Other (please specify)"}
-  ];
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    this.assistanceForm.get('issue').disable()
-    this.assistanceForm.controls.orders.patchValue(this.orders[0].id);
+    this.addCheckboxes();
+  }
+
+  addCheckboxes() {
+    this.requestlist.forEach((o,i) => {
+      const control = new FormControl(i===0);
+      (this.assistanceForm.controls.requests as FormArray).push(control);
+    })
   }
 
   assistanceForm = new FormGroup({
     issue: new FormControl(),
-    orders: new FormControl({orders: ['']})
+    requests: new FormArray([]),
   });
 
-  get_request_string = (request_id) => {
-    return this.orders[request_id-1].request
-  }
-  
-  alertme = () => {
-    let id_current = this.assistanceForm.value['orders'];
-    if (id_current == this.orders.length) this.assistanceForm.get('issue').enable()
-    else this.assistanceForm.get('issue').disable()
-  }
-
   onFormSubmit() {
-    let string = '';
-    let id_recieve = this.assistanceForm.value['orders'];
-    if (id_recieve != this.orders.length) string = this.get_request_string(id_recieve)
-    else string = this.assistanceForm.value['issue']
+    const selectedrequestids = this.assistanceForm.value.requests.map((v,i) => (v ? this.requestlist[i].id : null)).filter(v  => v !== null);
+    var string = ''
+    for (let i = 0; i < selectedrequestids.length; i++){
+      string += this.requestlist[selectedrequestids[i]-1].request
+      string += "\n"
+    };
+    if (this.assistanceForm.value['issue'] !== null) string += this.assistanceForm.value['issue']
     let request_data = {
       'problem': string
     }
