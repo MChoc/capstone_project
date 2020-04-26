667841ac94156face1e7719c0ad3a744d2b9eca8
tk <tripta1611@gmail.com>
2020-04-27 00:37:26 +1000

    add setup file

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# get filepath of project root dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
+# install dependencies
+sudo apt install npm python3.8 libpq-dev python3-pip python3-virtualenv
+cd front-end
+npm install
+
+cd $DIR
+PYTHON_PATH=`which python3.8`
+echo -e "\e[92mPYTHON PATH is: $PYTHON_PATH, creating virtual environment\e[39m"
+if [ -d .venv ]; then
+    virtualenv --python=$PYTHON_PATH .venv
+fi 
+source .venv/bin/activate
+echo -e "\e[92mInstalling requirements.txt\e[39m"
+pip install -r requirements.txt
+cd waiting
+echo -e "\e[92mMigrating and creating dummy data\e[39m"
+./clean.sh 
