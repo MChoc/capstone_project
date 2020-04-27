e438deb615c7da52722683a6f6a404a0ebd0398e
root <tripta1611@gmail.com>
2020-04-28 00:21:30 +1000

    check npm and node versions

--- setup.sh
+++ setup.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
+
+
+
 # get filepath of project root dir
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
@@ -7,6 +41,31 @@ cd $DIR
 # install dependencies
 sudo apt install npm python3.8 libpq-dev python3-pip virtualenv python3.8-dev
 cd front-end
+
+# check npm version
+currentver_npm="$(npm -v)"
+requiredver_npm="6.10.0"
+vercomp  $currentver_npm $requiredver_npm 
+if [ $? == 2 ]; then
+    echo -e "\e[31mThe minimum npm version requirement is 6.1.0, please upgrade your npm version and try again.\e[39m"
+    exit 1
+else
+    echo -e "\e[92mnpm requirements met. Continuing..\e[39m"
+fi
+
+# check node version
+currentver_node="$(node -v)"
+requiredver_node="10.00.0"
+# remove initial 'v' from node version
+currentver_node=${currentver_node:1}
+vercomp  $currentver_node $requiredver_node 
+if [ $? == 2 ]; then
+    echo -e "\e[31mThe minimum npm version requirement is 10.0.0, please upgrade your node version and try again.\e[39m"
+    exit 1
+else
+    echo -e "\e[92mnode requirements met. Continuing..\e[39m"
+fi
+
 npm install
 
 cd $DIR
